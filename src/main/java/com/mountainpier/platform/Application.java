89c757976a7e6e7bce631c1d83d92fe69786bc3c
matthewpoletin <contact@matthewpoletin.ru>
2018-06-01 11:23:40 +0300

    Rest repo auto config excluded

--- src/main/java/com/mountainpier/platform/Application.java
+++ src/main/java/com/mountainpier/platform/Application.java
@@ -2,8 +2,9 @@ package com.mountainpier.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
